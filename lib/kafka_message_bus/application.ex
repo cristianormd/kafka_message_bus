b2668fa8e314f5c1dd844c10a880e89ee521feda
Cristiano Dias <cristiano.dias@deepx.it>
2017-09-15 10:53:01 -0300

    Application starts consumer for ALL topics

--- lib/kafka_message_bus/application.ex
+++ lib/kafka_message_bus/application.ex
@@ -1,15 +1,27 @@
 defmodule KafkaMessageBus.Application do
   use Application
 
+
   alias KafkaMessageBus.Config
 
   def start(_type, _args) do
     import Supervisor.Spec
 
-    [supervisor(Kaffe.Consumer, [])]
+    consumer =  :kaffe
+              |> Application.get_env(:consumer)
+              |> Keyword.put(:topics, topic_list())
+    Application.put_env(:kaffe, :consumer, consumer)
+
+    [supervisor(Kaffe.GroupMemberSupervisor, [])]
     |> Supervisor.start_link(
       strategy: :one_for_one,
       name: KafkaMessageBus.Supervisor
     )
+  end
+
+  def topic_list do
+    Application.get_env(:kafka_message_bus, :consumers)
+    |> Enum.map(fn({topic, processor}) -> topic end)
+    |> Enum.reduce([], fn(topic, acc) -> acc ++ [topic] end)
   end
 end
