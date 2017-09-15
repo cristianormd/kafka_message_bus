c19c874faacd8b3ba585763b02af2719918195f8
Gabriel Alves <g04lves@gmail.com>
2017-09-15 09:38:43 -0300

    Changing lib to not use kafka_ex

--- lib/kafka_message_bus/consumer.ex
+++ lib/kafka_message_bus/consumer.ex
@@ -1,28 +1,13 @@
-defmodule KafkaMessageBus.Consumer do
-  use KafkaEx.GenConsumer
-  alias KafkaEx.Protocol.Fetch.Message
-  alias KafkaMessageBus.Config
-  require Logger
-
-  def init(topic, partition) do
-    Logger.debug("Initialize worker for #{topic}/#{partition}")
-
-    {:ok, %{
-      topic: topic,
-      partition: partition,
-      message_processor: Config.get_message_processor(topic)
-    }}
-  end
-
-  def handle_message_set(message_set, state) do
-    for %Message{key: key, value: value} <- message_set do
+defmodule KafkaMessageBus.MessageProcessor do
+  def handle_messages(messages) do
+    for message = {key: key, value: value} <- messages do
+      message_processor = Config.get_message_processor(message.topic)
       Logger.debug "Got message: KEY: #{key}, VALUE: #{value}"
 
       value
       |> Poison.decode!
-      |> state.message_processor.process(key)
+      |> message_processor.process(key)
     end
-
-    {:async_commit, state}
+    :ok
   end
 end
