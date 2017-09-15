6992478d5a480041039dc56a1ef14541d9a46377
Cristiano Dias <cristiano.dias@deepx.it>
2017-09-15 10:59:12 -0300

    Consumer runs proper process function

--- lib/kafka_message_bus/consumer.ex
+++ lib/kafka_message_bus/consumer.ex
@@ -1,13 +1,28 @@
 defmodule KafkaMessageBus.MessageProcessor do
-  def handle_messages(messages) do
-    for message = {key: key, value: value} <- messages do
-      message_processor = Config.get_message_processor(message.topic)
-      Logger.debug "Got message: KEY: #{key}, VALUE: #{value}"
+  @topics_and_processors Application.get_env(:kafka_message_bus, :consumers)
+  require Logger
 
-      value
-      |> Poison.decode!
-      |> message_processor.process(key)
+  def handle_messages(messages) do
+    for message = %{key: key, value: value} <- messages do
+      Logger.debug "Got message: KEY: #{key}, VALUE: #{value}"
+      process_message(message)
     end
+    :ok
+  end
+
+  def process_message(message) do
+    @topics_and_processors
+    |> Enum.map(fn config -> execute_message(message, config) end)
+  end
+
+  def execute_message(message = %{topic: topic}, {topic, message_processor}) do
+    message.value
+    |> Poison.decode!
+    |> message_processor.process(message.key)
+    :ok
+  end
+
+  def execute_message(_, _) do
     :ok
   end
 end
